f29551293de725790093591c69aff472a6bd845d
BKY\bucky <buckyhelmsmash@gmail.com>
2022-04-19 17:27:06 +0700

    Add: Routing init

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
+      <Link 
+        prefetch={false} 
+        href={{
+          pathname: '/[username]',
+          query: { username: 'jeff6969' }
+        }}
+      >
+        <a>Jeff{`&apos`}s Profile</a>
+      </Link>
     </div>
   )
 }
